84b784e74a56f0abc68417b7227ee7f8bd4690f8
wonmooncha <wmcha@scgs.co.kr>
2021-08-06 10:41:50 +0900

    cra first commit - ignore add

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactwla 4%
         </a>
       </header>
     </div>
